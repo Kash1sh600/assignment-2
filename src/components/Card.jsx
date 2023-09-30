c1655623e24e9f3d44c41b00dd5e635edb0515f6
Kash1sh600 <136251532+Kash1sh600@users.noreply.github.com>
2023-09-30 22:39:55 +0530

    Add files via upload

---
+++ src/components/Card.jsx
@@ -0,0 +1,218 @@
+const Card = () => {
+  return (
+    
+    
+    <main class="ml-3  ">
+            <div class="mt-0 grid grid-cols-6 gap gap-x-3 gap-y-7">
+                
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md min-w-full">
+                        <img src="https://source.unsplash.com/random?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden flex " >
+                            <img src="https://source.unsplash.com/random?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md">
+                        <img src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden" >
+                            <img src="https://images.unsplash.com/photo-1570724061670-86a53c509dee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md">
+                        <img src="https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bc01c83c3da0425e9baa6c7a9204af81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden flex" >
+                            <img src="/img/image1.jpg" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md min-w-full">
+                        <img src="https://source.unsplash.com/random?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden flex " >
+                            <img src="https://source.unsplash.com/random?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md">
+                        <img src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden" >
+                            <img src="https://images.unsplash.com/photo-1570724061670-86a53c509dee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md">
+                        <img src="https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bc01c83c3da0425e9baa6c7a9204af81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden flex" >
+                            <img src="/img/image1.jpg" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md min-w-full">
+                        <img src="https://source.unsplash.com/random?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden flex " >
+                            <img src="https://source.unsplash.com/random?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md">
+                        <img src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden" >
+                            <img src="https://images.unsplash.com/photo-1570724061670-86a53c509dee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md">
+                        <img src="https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bc01c83c3da0425e9baa6c7a9204af81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden flex" >
+                            <img src="/img/image1.jpg" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md min-w-full">
+                        <img src="https://source.unsplash.com/random?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden flex " >
+                            <img src="https://source.unsplash.com/random?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md">
+                        <img src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden" >
+                            <img src="https://images.unsplash.com/photo-1570724061670-86a53c509dee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-68">
+                    <div class="h-25 w-full bg-gray-700 overflow-hidden rounded-md">
+                        <img src="https://images.unsplash.com/5/unsplash-kitsune-4.jpg?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bc01c83c3da0425e9baa6c7a9204af81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt=""/>
+                    </div>
+                    <div class="flex mt-2">
+                        <div class="h-10 w-10 bg-blue-300 rounded-full flex-shrink-0 overflow-hidden flex" >
+                            <img src="/img/image1.jpg" alt=""/>
+                        </div>
+                        <div class="ml-2">
+                            <div class="text-sm font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing.</div>
+                            <div class="text-xs text-gray-600">
+                                <p>Channel Name</p>
+                                <p>9 hours ago &middot; 87k views</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+            </div>            
+    </main>
+    
+  )
+}
+
+export default Card
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div >
+      <img class="md:w-full"src="/public/img/image9.jpg" alt="Advertisement"/>
+    </div>
+  )
+}
+
+export default Hero
